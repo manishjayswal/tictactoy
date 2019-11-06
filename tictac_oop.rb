4851a3d689cebc5f893aa11709ded8089e6f8b27
Manish-Jayswal <jayswalmp@gmail.com>
2019-11-06 14:49:06 +0545

    some methods moved to board

--- tictac_oop.rb
+++ tictac_oop.rb
@@ -1,33 +1,9 @@
 class TicTac
   def initialize
     @table_dimension = 3
-    @board_object = Board.new(@table_dimension)
+    @board = Board.new(@table_dimension)
     @current_player = 'player_X'
     @current_player_symbol = 'X'
-  end
-
-  def get_box_positions
-    loop do
-      position = ask_for_box_position
-      if valid_move?(position)
-        break if @board_object.fill_box(position, @current_player_symbol)
-      else
-        puts 'Invalid Position!!'
-      end
-    end
-  end
-
-  def ask_for_box_position
-    puts "#{@current_player},
-      enter the position where you want to place your mark:"
-    gets.chomp.to_i
-  end
-
-  def valid_move?(position)
-    total_number_of_box = @table_dimension * @table_dimension
-    return true if (position >= 0) && (position < total_number_of_box)
-
-    false
   end
 
   def check_won?
@@ -36,8 +12,8 @@ class TicTac
                              else
                                'O'
                              end
-    if @board_object.has_won?(@current_player_symbol)
-      puts "#{@current_player} won it."
+    if @board.has_won?(@current_player_symbol)
+      puts "Congratulations!!! #{@current_player} won this game."
       return true
     else
       return false
@@ -45,7 +21,7 @@ class TicTac
   end
 
   def check_draw?
-    return true if @board_object.full? && !check_won?
+    return true if @board.full? && !check_won?
 
     false
   end
@@ -61,15 +37,15 @@ class TicTac
   end
 
   def start_game
-    @board_object.display_board
-    get_box_positions
-    @board_object.display_board
+    @board.display_board
+    @board.get_box_positions(@current_player_symbol, @current_player)
+    @board.display_board
     (1...@table_dimension * @table_dimension).each do
       return if check_won?
 
       toggle_players
-      get_box_positions
-      @board_object.display_board
+      @board.get_box_positions(@current_player_symbol, @current_player)
+      @board.display_board
     end
     puts 'This Game is Draw!!!' if check_draw?
   end
@@ -81,14 +57,14 @@ end
 class Board
   def initialize(table_dimension)
     @table_dimension = table_dimension
-    @board = Array.new(@table_dimension)
+    @box = Array.new(@table_dimension)
   end
 
   def display_board
     (0...@table_dimension).each do |row|
       (0...@table_dimension).each do |column|
-        if !@board[@table_dimension * row + column].nil?
-          print "\t#{@board[@table_dimension * row + column]}"
+        if !@box[@table_dimension * row + column].nil?
+          print "\t#{@box[@table_dimension * row + column]}"
         else
           print "\t#{@table_dimension * row + column} "
         end
@@ -107,7 +83,7 @@ class Board
 
   def fill_box(position, symbol)
     if box_location_valid?(position)
-      @board[position] = symbol
+      @box[position] = symbol
       true
     else
       false
@@ -127,10 +103,10 @@ class Board
   end
 
   def position_available?(position)
-    if @board[position].nil?
+    if @box[position].nil?
       true
     else
-      puts 'There is already a symbol there!'
+      puts 'Invalid Move! There is already a symbol there!'
     end
   end
 
@@ -144,7 +120,7 @@ class Board
   def winning_diagonal1?(current_symbol)
     counter = 0
     (0...@table_dimension).each do |row_index|
-      if current_symbol == @board[row_index * (@table_dimension + 1)]
+      if current_symbol == @box[row_index * (@table_dimension + 1)]
         counter += 1
       end
     end
@@ -156,7 +132,7 @@ class Board
   def winning_diagonal2?(current_symbol)
     counter = 0
     (0...@table_dimension).each do |row_index|
-      if current_symbol == @board[(row_index + 1) * (@table_dimension - 1)]
+      if current_symbol == @box[(row_index + 1) * (@table_dimension - 1)]
         counter += 1
       end
     end
@@ -169,7 +145,7 @@ class Board
     (0...@table_dimension).each do |row_index|
       counter = 0
       (0...@table_dimension).each do |column_index|
-        if current_symbol == @board[row_index + column_index * @table_dimension]
+        if current_symbol == @box[row_index + column_index * @table_dimension]
           counter += 1
         end
       end
@@ -182,7 +158,7 @@ class Board
     (0...@table_dimension).each do |row_index|
       counter = 0
       (0...@table_dimension).each do |column_index|
-        if current_symbol == @board[row_index * @table_dimension + column_index]
+        if current_symbol == @box[row_index * @table_dimension + column_index]
           counter += 1
         end
       end
@@ -194,12 +170,36 @@ class Board
   def full?
     counter = 0
     (0...@table_dimension * @table_dimension).each do |i|
-      counter += 1 unless @board[i].nil?
+      counter += 1 unless @box[i].nil?
     end
     return true if counter == @table_dimension * @table_dimension
 
     false
   end
+
+  def get_box_positions(current_symbol, current_player)
+    loop do
+      position = ask_for_box_position(current_player)
+      if valid_move?(position)
+        break if fill_box(position, current_symbol)
+      else
+        puts 'Invalid Move!! Out of bound.'
+      end
+    end
+  end
+
+  def ask_for_box_position(current_player)
+    puts "#{current_player}, enter the position where you want to place your mark:"
+    gets.chomp.to_i
+  end
+
+  def valid_move?(position)
+    total_number_of_box = @table_dimension * @table_dimension
+    return true if (position >= 0) && (position < total_number_of_box)
+
+    false
+  end
+
 end
-tictac_object = TicTac.new
-tictac_object.start_game
+tictac = TicTac.new
+tictac.start_game
